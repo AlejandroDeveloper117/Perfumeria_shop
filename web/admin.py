6922b0e420c7f4e3d6eaeb55fcb315a17db7b4c6
AlejandroDeveloper117 <androgarcia117@gmail.com>
2024-09-28 19:59:21 -0600

    Se crea el proyecto con algunas configuraciones basicas

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria,Producto
+
+admin.site.register(Categoria)
+#admin.site.register(Producto)
+
+@admin.register(Producto)
+class Productoadmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'Categoria' )
+    list_editable = ('precio', ) # para el panel de adminsitracion poder cambiar el precio de manera muy facil
